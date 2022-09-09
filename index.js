14a8d92d113baa27164624c7cb6fedb0cf6350a5
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-09-08 21:50:12 -0500

    Added schema and db along w/ a seeds file.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const inquirer = require('inquirer');
+const db = require('./db/connection');
 
-
+// Start connection to db
+db.connect(err => {
+    if(err) throw err;
+    console.log("Database Connected");
+});
 console.log("index connection...");
