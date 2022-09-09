96c754eeca2e73770201a37e6b1513f4348f6ffd
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-09-09 01:09:17 -0500

    Added READ functionality for all classes. Restructured questions and wrote swtich statement to sort through answers

--- utils/questions.js
+++ utils/questions.js
@@ -5,6 +5,26 @@ const Role = require('../lib/Role.js');
 
 // Object declarations
 let department = new Deparment();
+let role = new Role();
+let employee = new Employee();
+
+function continueApp() {
+    console.log("We are continuing");
+    inquirer.prompt([
+                {
+                    type: 'confirm',
+                    name: "confirm",
+                    message: 'Would you like to take another action?'
+                }
+        
+            ])
+                .then((data) => {
+                    console.log(data);
+                    if(data.confirm) {
+                        questions();
+                    }
+                });
+}
 
 const questions = () => {
     inquirer.prompt([{
@@ -12,18 +32,38 @@ const questions = () => {
             name: 'choice',
             message: 'What would you like to do?',
             choices: ['View Departments', new inquirer.Separator(), 'View Roles', new inquirer.Separator(), 'View Employees', new inquirer.Separator(), 'Add a Department', new inquirer.Separator(), 'Add a Role', new inquirer.Separator(), 'Add an Employee', new inquirer.Separator(), 'Update an Employee Role']
-        },
-        {
-            type: 'confirm',
-            name: "confirm",
-            message: 'Would you like to take another action?'
         }
-
     ])
         .then((data) => {
-            console.log(data.confirm);
-            if(data.confirm) {
-                questions();
+            switch(data.choice) {
+                case "View Departments":
+                    department.viewDepartments();
+                    return;
+                case "View Roles":
+                    role.viewRoles();
+                    break;
+                case "View Employees":
+                    employee.viewEmployees();
+                    break;
+                default:
+                    console.log("This didn't work");
+            }
+        })
+
+
+        .then(() => {
+            return inquirer.prompt([
+                {
+                    type: 'confirm',
+                    name: "confirm",
+                    message: 'Would you like to take another action?'
+                }
+        
+            ])
+        })
+        .then((response) => {
+            if(response.confirm) {
+               return questions();
             }
         });
 };
