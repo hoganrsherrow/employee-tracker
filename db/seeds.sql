813c5910046061d4e64f5120194bb0e1737fa9a9
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-09-08 22:11:42 -0500

    Added seeds to populate tables

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department
+VALUES
+    (1, 'Product'),
+    (2, 'Design'),
+    (3, 'Marketing'),
+    (4, 'Sales');
+
+INSERT INTO role
+VALUES  
+    (1, 'Software Development Engineer', 125000.00, 1),
+    (2, 'Web Developer', 80000.00, 3),
+    (3, 'UI/UX Designer', 60000.00, 2),
+    (4, 'Salesman', 70000.00, 4);
+
+INSERT INTO employee
+VALUES
+    (1, 'Hogan', 'Sherrow', 1, null);
